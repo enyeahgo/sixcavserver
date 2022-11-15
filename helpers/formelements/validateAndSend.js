b84a56ca4f56db11e4587c86b0167a7bcaeaacfb
Inigo Orosco <enyeahgo@gmail.com>
2022-11-15 14:04:11 +0800

    New Commit

--- helpers/formelements/validateAndSend.js
+++ helpers/formelements/validateAndSend.js
@@ -6,11 +6,15 @@ let validateAndSend = (url, btnid, inputArr, staff) => {
     if(i.split('-')[0] == 's') {
       inputs += `(document.getElementById('${i.split('-')[1]}').selectedIndex > 0) && `;
       postBody += `"${i.split('-')[1]}": document.getElementById('${i.split('-')[1]}').value, `;
-      resetForm += `document.getElementById('${i.split('-')[1]}').selectedIndex = 0; `
+      if(i.split('-')[1] != 'staff' && i.split('-')[1] != 'type') {
+        resetForm += `document.getElementById('${i.split('-')[1]}').selectedIndex = 0; `
+      }
     } else {
       inputs += `(document.getElementById('${i}').value != '') && `;
       postBody += `"${i}": document.getElementById('${i}').value, `;
-      resetForm += `document.getElementById('${i}').value = ''; `
+      if(i != 'staff' && i != 'type') {
+        resetForm += `document.getElementById('${i}').value = ''; `
+      }
     }
   });
   inputs += 'true';
@@ -31,6 +35,11 @@ let validateAndSend = (url, btnid, inputArr, staff) => {
           toast('Please fill-out all items.', 'error');
         }
       });
+
+      socket.on('dbchanged', staff => {
+				console.log(staff);
+			});
+
     </script>
   `;
 };
